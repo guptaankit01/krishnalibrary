fcb51a2f945b0e59cd8d4405b00739ceb27332a7
guptaankit01 <akedufiles@gmail.com>
2024-08-14 10:48:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ elif st.session_state['page'] == 'Girls':
         "<span style='color:green; font-weight:bold;'>To all the girls studying here: Embrace your strength, pursue your passions with determination, and let this library be a sanctuary where your dreams take flight. Your dedication and hard work will pave the way for a brighter future.</span>",
         unsafe_allow_html=True
     )
-    st.write("avl=seat available, res=seat taken")
+    st.write("vacant_space=seat available, blue_tick=seat reserved")
 
     if st.button('Check Available Slots'):
         try:
@@ -119,7 +119,9 @@ elif st.session_state['page'] == 'Girls':
 
 elif st.session_state['page'] == 'Boys':
     st.header("Boys Section")
-    st.write("avl=seat available, res=seat taken")
+   # st.write("avl=seat available, res=seat taken")
+    st.write("vacant_space=seat available, blue_tick=seat reserved")
+
 
     if st.button('Check Available Slots'):
         try:
